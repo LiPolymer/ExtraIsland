14a0fd17bbbf4fcb59b94f4e5eb6fa1867d9625d
LiPolymer <fish@lipoly.ink>
2025-03-31 23:51:41 +0800

    feat: add rule TodayIs

--- ExtraIsland/Automations/Register.cs
+++ ExtraIsland/Automations/Register.cs
@@ -1,6 +1,7 @@
 ﻿using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Extensions.Registry;
 using ExtraIsland.Automations.Actions;
+using ExtraIsland.Automations.Rules;
 using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,9 +20,12 @@ public class Register : IHostedService {
     public static void Claim(IServiceCollection services) {
         // 行动
         services.AddAction<MainWindowOperatorConfig, MainWindowOperator>(
-            "extraisland.action.mainWindowOperator", 
+            "extraIsland.action.mainWindowOperator", 
             "主窗口操作器", 
             PackIconKind.DesktopWindows);
+        // 规则
+        services.AddRule<TodayIsConfig, TodayIs>
+            ("extraIsland.rule.todayIs", "今天是", PackIconKind.Calendar);
     }
     
     /// <summary>
@@ -31,8 +35,10 @@ public class Register : IHostedService {
     /// <param name="rulesetService">规则集服务</param>
     public Register(IActionService actionService, IRulesetService rulesetService) {
         //行动
-        actionService.RegisterActionHandler("extraisland.action.mainWindowOperator",MainWindowOperator.Action);
-        actionService.RegisterRevertHandler("extraisland.action.mainWindowOperator",MainWindowOperator.Revert);
+        actionService.RegisterActionHandler("extraIsland.action.mainWindowOperator",MainWindowOperator.Action);
+        actionService.RegisterRevertHandler("extraIsland.action.mainWindowOperator",MainWindowOperator.Revert);
+        //规则
+        rulesetService.RegisterRuleHandler("extraIsland.rule.todayIs",TodayIs.Rule);
     }
     
     public Task StartAsync(CancellationToken _) {
---
+++ ExtraIsland/Automations/Rules/TodayIs.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.Controls;
+
+namespace ExtraIsland.Automations.Rules;
+
+public partial class TodayIs {
+    public TodayIs() {
+        InitializeComponent();
+    }
+
+    void OnTargetTypeChanged() {
+        this.BeginInvoke((() => {
+            DayOfWeekSelector.Visibility = Settings.Target == TodayIsConfig.TargetType.DayOfWeek 
+                ? Visibility.Visible : Visibility.Collapsed;
+        }));
+    }
+
+    public List<TodayIsConfig.TargetType> TargetTypes { get; } = [
+        TodayIsConfig.TargetType.DayOfWeek,
+        TodayIsConfig.TargetType.Weekday,
+        TodayIsConfig.TargetType.Weekend
+    ];
+
+    public static List<DayOfWeek> DayOfWeeks { get; } = [
+        DayOfWeek.Monday,
+        DayOfWeek.Tuesday,
+        DayOfWeek.Wednesday,
+        DayOfWeek.Thursday,
+        DayOfWeek.Friday,
+        DayOfWeek.Saturday,
+        DayOfWeek.Sunday
+    ];
+
+    static List<DayOfWeek> DayOfWeekdays { get; } = [
+        DayOfWeek.Monday,
+        DayOfWeek.Tuesday,
+        DayOfWeek.Wednesday,
+        DayOfWeek.Thursday,
+        DayOfWeek.Friday
+    ];
+
+    static List<DayOfWeek> DayOfWeekend { get; } = [
+        DayOfWeek.Saturday,
+        DayOfWeek.Sunday
+    ];
+    void TodayIs_OnLoaded(object sender,RoutedEventArgs e) {
+        OnTargetTypeChanged();
+        Settings.TargetTypeChanged += OnTargetTypeChanged;
+    }
+    void TodayIs_OnUnloaded(object sender,RoutedEventArgs e) {
+        Settings.TargetTypeChanged -= OnTargetTypeChanged;
+    }
+    public static bool Rule(object? rawConfig) {
+        TodayIsConfig config = (TodayIsConfig)rawConfig!;
+        return config.Target switch {
+            TodayIsConfig.TargetType.DayOfWeek => DateTime.Now.DayOfWeek == config.TargetDayOfWeek,
+            TodayIsConfig.TargetType.Weekday => DayOfWeekdays.Contains(DateTime.Now.DayOfWeek),
+            TodayIsConfig.TargetType.Weekend => DayOfWeekend.Contains(DateTime.Now.DayOfWeek),
+            _ => false
+        };
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class TodayIsConfig : ObservableRecipient {
+
+    public event Action? TargetTypeChanged;
+
+    TargetType _targetType = TargetType.Weekday;
+    public TargetType Target {
+        get => _targetType;
+        set {
+            if (_targetType == value) return;
+            _targetType = value;
+            TargetTypeChanged?.Invoke();
+        }
+    }
+    public DayOfWeek TargetDayOfWeek { get; set; } = DayOfWeek.Monday;
+
+    public enum TargetType {
+        [Description("工作日")]
+        Weekday,
+        [Description("周末")]
+        Weekend,
+        [Description("星期...")]
+        DayOfWeek
+    }
+}
