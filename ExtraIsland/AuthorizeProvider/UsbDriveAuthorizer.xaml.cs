b72235ef78a51860897d5ce814b4d4139ddef076
LiPolymer <fish@lipoly.ink>
2025-04-20 14:10:38 +0800

    feat:file credential mode

--- ExtraIsland/AuthorizeProvider/UsbDriveAuthorizer.xaml.cs
+++ ExtraIsland/AuthorizeProvider/UsbDriveAuthorizer.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using ClassIsland.Core;
 using ClassIsland.Core.Attributes;
 using ClassIsland.Core.Controls.CommonDialog;
@@ -38,45 +40,81 @@ public partial class UsbDriveAuthorizer {
     void OnUsbInserted(object? sender,string driveLetter) {
         if (IsEditingMode & EditLock) return;
         EditLock = true;
+        OperationFinished = false;
+        ToggleButton setter = (ModeSettingsControl.Switcher as ToggleButton)!;
+        bool isFileMode = setter.IsChecked!.Value;
+        // TODO:多分区支持
+        string path = $"{driveLetter}\\.verify.extraisland.nfo";
         WindowsUtils.UsbDriveInfo info = WindowsUtils.FindUsbDriveByLetter(driveLetter);
-        if (info.SerialNumber == null) {
-            if (IsEditingMode)
-            {
+        if (info.SerialNumber == null & !isFileMode) {
+            if (IsEditingMode) {
                 CommonDialog.ShowError("此设备S/N异常,不能使用该设备!");
                 EditLock = false;
-            } 
-            else
-            {
+            } else {
                 OperationFinished = true;
             }
             return;
         }
         if (IsEditingMode) {
-            Settings.PassHash = EiUtils.Sha256EncryptString(info.SerialNumber!);
+            if (isFileMode) {
+                if (!File.Exists(path)) {
+                    File.WriteAllText(path, Guid.NewGuid().ToString());
+                }
+                string? hash = GetCredentialHash(path);
+                if (hash != null) {
+                    Settings.PassHash = hash;
+                } else {
+                    CommonDialog.ShowError("读取密钥文件时遇到错误");
+                    return;
+                }
+            } else {
+                Settings.PassHash = EiUtils.Sha256EncryptString(info.SerialNumber!);
+            }
             Operating = false;
             OperationFinished = true;
-        } else if(Settings.PassHash == EiUtils.Sha256EncryptString(info.SerialNumber!)) {
-            CompleteAuthorize();
-        } else {
-            OperationFinished = true;
+            Settings.IsFileModeEnabled = isFileMode;   
+        } else if (Settings.IsFileModeEnabled) {
+            if (Settings.PassHash == GetCredentialHash(path)) {
+                CompleteAuthorize();
+            } else {
+                OperationFinished = true;   
+            }
+        }
+        else {
+            if (Settings.PassHash == EiUtils.Sha256EncryptString(info.SerialNumber!)) {
+                CompleteAuthorize();
+            } else {
+                OperationFinished = true;   
+            }
         }
     }
-    
+
+    static string? GetCredentialHash(string path) {
+        return !File.Exists(path) ? null : EiUtils.Sha256EncryptString(File.ReadAllText(path));
+    }
+
     void UsbDriveAuthorizer_OnUnloaded(object sender,RoutedEventArgs e) {
         _monitor.UsbDriveInserted -= OnUsbInserted;
     }
-    void ButtonBase_OnClick(object sender,RoutedEventArgs e) {
+    void SetCredential(object sender,RoutedEventArgs e) {
         Operating = true;
         EditLock = false;
     }
     void VerifyButton_OnClick(object sender,RoutedEventArgs e) {
-        if (WindowsUtils.ScanUsbDrive().Any(info => Settings.PassHash == EiUtils.Sha256EncryptString(info.SerialNumber!))) {
+        if (Settings.IsFileModeEnabled) {
+            foreach (WindowsUtils.UsbDriveInfo info in WindowsUtils.ScanUsbDrive()) {
+                if (info.DriveLetter == null) continue;
+                if (info.DriveLetter.All(letter => Settings.PassHash != GetCredentialHash($"{letter}\\.verify.extraisland.nfo"))) continue;
+                CompleteAuthorize();
+                return;
+            }
+        } else if (WindowsUtils.ScanUsbDrive().Any(info => Settings.PassHash == EiUtils.Sha256EncryptString(info.SerialNumber!))) {
             CompleteAuthorize();
             return;
         }
         OperationFinished = true;
     }
     void UsbDriveAuthorizer_OnLoaded(object sender,RoutedEventArgs e) {
-        
+
     }
 }
--- ExtraIsland/AuthorizeProvider/UsbDriveAuthorizerSettings.cs
+++ ExtraIsland/AuthorizeProvider/UsbDriveAuthorizerSettings.cs
@@ -2,5 +2,6 @@
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public class UsbDriveAuthorizerSettings {
-    public string PassHash { get; set; }
+    public string PassHash { get; set; } = string.Empty;
+    public bool IsFileModeEnabled { get; set; }
 }
