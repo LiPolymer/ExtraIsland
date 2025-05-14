fdd88bf5cfba7a5f28938e51ff842fa8fe1cc43e
LiPolymer <fish@lipoly.ink>
2025-05-14 13:29:39 +0800

    ui: use grayscale vector logo as watermark in automations

---
+++ ExtraIsland/Controls/FullLogoGrayScale.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace ExtraIsland.Controls;
+
+public partial class FullLogoGrayScale : UserControl {
+    public FullLogoGrayScale() {
+        InitializeComponent();
+    }
+}
