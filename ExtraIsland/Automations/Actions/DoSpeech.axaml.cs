4f59c7284f8e904012fc3c1639eb882662edf0a5
Nikoa <diannwork@outlook.com>
2026-07-19 18:44:14 +0800

    merge: pull request #87 from diann34/v1
    feat:添加“语音播报”行动&启用在非Windows平台的IDE补全支持

---
+++ ExtraIsland/Automations/Actions/DoSpeech.axaml.cs
@@ -0,0 +1,37 @@
+using ClassIsland.Core.Abstractions.Controls;
+using ClassIsland.Core.Abstractions.Automation;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Abstractions.Services.SpeechService;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Threading.Tasks;
+using System;
+using ClassIsland.Core;
+using ClassIsland.Core.Controls;
+using ClassIsland.Shared;
+using ExtraIsland.Shared;
+
+namespace ExtraIsland.Automations.Actions;
+
+public partial class DoSpeechSettingsControl : ActionSettingsControlBase<DoSpeechSettings> {
+    public DoSpeechSettingsControl() {
+        InitializeComponent();
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class DoSpeechSettings : ObservableRecipient {
+    public string Text { get; set; } = string.Empty;
+}
+
+[ActionInfo("extraIsland.action.doSpeech", "语音播报", "\uED53")]
+public class DoSpeechAction : ActionBase<DoSpeechSettings> {
+    protected override Task OnInvoke() {
+        base.OnInvoke();
+        string text = Settings.Text;
+        if (string.IsNullOrWhiteSpace(text))
+            throw new InvalidOperationException("语音播报内容不能为空");
+        IAppHost.GetService<ISpeechService>().EnqueueSpeechQueue(text);
+        return Task.CompletedTask;
+    }
+}
+
